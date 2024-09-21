3c02cf57f97138223222f598a7cd437754f561b4
Phil <alexis.hochart@milixis.fr>
2024-09-21 14:19:04 +0200

    SumAllTails func & tests

--- slice/slice.go
+++ slice/slice.go
@@ -6,3 +6,24 @@ func Sum(list []int) (result int) {
 	}
 	return result
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	sums := make([]int, len(numbersToSum))
+	for i, slice := range numbersToSum {
+		sums[i] = Sum(slice)
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	sums := make([]int, len(numbersToSum))
+	for i, slice := range numbersToSum {
+		if len(slice) == 0 {
+			sums[i] = 0
+		} else {
+			sums[i] = Sum(slice[1:])
+		}
+	}
+	return sums
+}
--- slice/slice_test.go
+++ slice/slice_test.go
@@ -1,6 +1,9 @@
 package slice
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 // Test sum on Array
 func TestSum(t *testing.T) {
@@ -13,5 +16,41 @@ func TestSum(t *testing.T) {
 		if want != got {
 			t.Errorf("want %d, got %d, given %v", want, got, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("Testing SumAll", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		//if !reflect.DeepEqual(got, want) {
+		//	t.Errorf("got %v, want %v", got, want)
+		//}
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+
+	t.Run("Testing TestSumAllTails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 9})
+		want := []int{5, 9}
+		checkSums(t, got, want)
+	})
+	t.Run("Testing sum empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
 	})
 }
